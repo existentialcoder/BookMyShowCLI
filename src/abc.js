37a637daa9bd950986b8ed1aa74a60aeb5617346
Shravan Balasubramanian <shravan.balasubramanian@freshworks.com>
2020-02-13 10:57:01 +0530

    Removed all

--- src/abc.js
+++
@@ -1,3 +0,0 @@
-import * as location from './getLocation';
-
-console.log(await location.getLocation());
--- utils/fileManipulations.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const SOURCE_FOLDER = 'api/';
-const fse = require('fs-extra');
-
-module.exports = {
-	writeFile : (data, fileName, user_location) => fse.outputFileSync(`${SOURCE_FOLDER}/${user_location}/${fileName}.json`, JSON.stringify(data)),
-	clearFolder : () => fse.remove(SOURCE_FOLDER)
-};
